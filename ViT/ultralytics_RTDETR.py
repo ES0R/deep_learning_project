b1f266a0c68fa28db618486130631ac4ce8eaba3
ES0R <emiloramovic@gmail.com>
2023-11-18 16:28:41 +0100

    yaboi

--- ViT/ultralytics_RTDETR.py
+++ ViT/ultralytics_RTDETR.py
@@ -1,30 +1,7 @@
 from ultralytics import RTDETR
 import pandas as pd
+from PIL import Image
 
-def save_results_to_csv(results, csv_filename):
-    # Prepare data for DataFrame
-    data = {}
-    for attr in dir(results):
-        if not attr.startswith('__') and not callable(getattr(results, attr)):
-            data[attr] = getattr(results, attr)
-
-    # Convert to DataFrame
-    results_df = pd.DataFrame([data])
-
-    # Save the DataFrame to a CSV file
-    results_df.to_csv(csv_filename, index=False)
-    print(f"Data converted to DataFrame and saved as '{csv_filename}'.")
-
-def print_attribute_from_csv(csv_filename, attribute):
-    # Read the CSV file into a DataFrame
-    results_df = pd.read_csv(csv_filename)
-
-    # Print the specified attribute
-    if attribute in results_df.columns:
-        print(f"{attribute}:")
-        print(results_df[attribute].iloc[0])
-    else:
-        print(f"Attribute '{attribute}' not found in the CSV file.")
 
 # Load a COCO-pretrained RT-DETR-l model
 model = RTDETR('rtdetr-l.pt')
@@ -35,16 +12,11 @@ model.info()
 # Train the model on the COCO8 example dataset
 results = model.train(data='coco8.yaml', epochs=1, imgsz=640, iou=0.9)
 
-# Save results to CSV
-save_results_to_csv(results, "results_data.csv")
+# Forward pass
+results = model('best_electric_luxury_car_bmw_i7.jpg')
 
-# Extract the actual data from the confusion matrix object
-conf_matrix_obj = results.confusion_matrix
-conf_matrix_data = conf_matrix_obj.matrix
+for r in results:
+    im_array = r.plot()  # plot a BGR numpy array of predictions
+    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+    im.save('results.jpg')
 
-# Convert to DataFrame and save to CSV
-conf_matrix_df = pd.DataFrame(conf_matrix_data)
-conf_matrix_df.to_csv("conf_matrix.csv", index=False)
-
-# Reading and printing the confusion matrix from CSV
-print_attribute_from_csv("conf_matrix.csv", "matrix")
--- YOLO/ultralytics_YOLO.py
+++ YOLO/ultralytics_YOLO.py
@@ -3,3 +3,6 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 results = model.train(data='coco8.yaml', epochs=10, imgsz=256)
+
+results = model('best_electric_luxury_car_bmw_i7.jpeg')
+
