a66eb9322f1f0844596255c898a0bc27cdc59260
ES0R <emiloramovic@gmail.com>
2023-12-07 03:34:10 +0100

    fuck asyhdjasokdasldæäsdknjahdisjakodlpaæsd.ada

--- custom/train.py
+++ custom/train.py
@@ -1,115 +1,257 @@
-import torch
-from torchvision import transforms
-from data_loader import COCODataset
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-import torch.nn as nn
-import torch.optim as optim
-from model import ObjectDetectionModel
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from PIL import Image
-import torch
-from torch.utils.data import Dataset
 import os
-import cv2
-from torchvision import transforms
 import json
-from PIL import Image, ImageOps
-import warnings
+import pandas as pd
+import numpy as np
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from pycocotools.coco import COCO
+import torch
+from torch.utils.data import DataLoader, Dataset
+import torchvision
+import cv2
+import matplotlib.pyplot as plt
+import random
+from torchvision import transforms
+import torch.nn as nn
+import torch
+from torch.optim import Adam
+from torch.nn import MSELoss, SmoothL1Loss, CrossEntropyLoss
+   
+from torchvision import datasets, models
+# import the necessary packages
+from torch.nn import Dropout
+from torch.nn import Identity
+from torch.nn import Linear
+from torch.nn import Module
+from torch.nn import ReLU
+from torch.nn import Sequential
+from torch.nn import Sigmoid
+from torch.optim import Adam
 
-warnings.filterwarnings('ignore', category=UserWarning, module='torchvision')
-########
+def coco_to_voc(coco_boxes):
+    voc_boxes = []
+    for box in coco_boxes:
+        x_min, y_min, width, height = box
+        x_max = x_min + width
+        y_max = y_min + height
+        voc_boxes.append([x_min, y_min, x_max, y_max])
+    return voc_boxes
 
-def collate_fn(batch):
-    # Filter out the placeholders
-    batch = [data for data in batch if data[0].nelement() != 0 and data[1].nelement() != 0 and data[2].nelement() != 0]
+def pad_to_fixed_size(boxes, labels, pad_size=93, pad_value=0):
+    # Create empty arrays with the fixed size, filled with pad_value for boxes or -1 for labels
+    padded_boxes = np.full((pad_size, 4), pad_value, dtype=np.float32)
+    padded_labels = np.full((pad_size,), 0, dtype=np.int64)  # Now 0 is used for background/padding
 
-    if len(batch) == 0:
-        return torch.zeros(0, 3, 224, 224), torch.zeros(0, 4), torch.zeros(0, dtype=torch.int64)
+    # Calculate how many items to copy from the original boxes and labels
+    num_boxes = min(len(boxes), pad_size)
+    if num_boxes > 0:
+        padded_boxes[:num_boxes] = boxes[:num_boxes]
+        padded_labels[:num_boxes] = labels[:num_boxes]
 
-    images, bboxes, labels = zip(*batch)
-    images = torch.stack(images, dim=0)
-    bboxes = tuple(bboxes)
-    labels = tuple(labels)
-
-    return images, bboxes, labels
-
-
-
-# Define image transformations
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.ToTensor()
-])
+    return padded_boxes, padded_labels
 
 
-# Paths to image folders and annotation files
-print("Starting data load")
-images_train_folder = '/dtu/blackhole/19/147257/deep/coco/images/train2017/' 
-images_val_folder = '/dtu/blackhole/19/147257/deep/coco/images/val2017/' 
-anno_train_file = '/dtu/blackhole/19/147257/deep/coco/annotations/instances_train2017.json'
-anno_val_file = '/dtu/blackhole/19/147257/deep/coco/annotations/instances_val2017.json'
+# Function to get a random training sample
+def get_random_sample(dataset):
+    random_index = 80 #random.randint(0, len(dataset) - 1)
+    image, boxes, labels = dataset[random_index]
+    image = image.permute(1, 2, 0).numpy()  # Convert from torch format to numpy format for plotting
+    return image, boxes, labels
 
-# Create dataset instances
-train_dataset = COCODataset(root=images_train_folder, annotation_file=anno_train_file, transform=transform)
-val_dataset = COCODataset(root=images_val_folder, annotation_file=anno_val_file, transform=transform)
+# Function to Save Plots
+def save_plot(image, voc_boxes, image_name):
+    fig, ax = plt.subplots(1)
+    ax.imshow(image)
+    for box in voc_boxes:
+        # Create a rectangle patch for the bounding box
+        x_min, y_min, x_max, y_max = box
+        rect = plt.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, fill=False, edgecolor='red', linewidth=2)
+        ax.add_patch(rect)
+    plt.axis('on')
+    plt.savefig(f'plots/{image_name}.png', bbox_inches='tight')
+    plt.close()
+# Define paths and parameters
+dataset_path = "/dtu/blackhole/19/147257/deep/coco/"
+anns_file_train = dataset_path + "annotations/instances_train2017.json"
+anns_file_val = dataset_path + "annotations/instances_val2017.json"
+image_size = 256
+batch_size = 8
+num_classes = 80
 
-# DataLoader parameters
-batch_size = 64
-shuffle = True
-num_workers = 4 # Adjust as per your machine's capability
+# Custom Dataset Class
+class CocoDataset(Dataset):
+    def __init__(self, root, annotation, transform=None):
+        self.root = root
+        self.coco = COCO(annotation)
+        self.image_ids = list(self.coco.imgs.keys())
+        self.transform = transform
 
-# Create DataLoaders
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, collate_fn=collate_fn)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=collate_fn)#,collate_fn=collate_fn)
-print("Finished data load")
+    def __getitem__(self, index):
+        image_id = self.image_ids[index]
+        image_info = self.coco.loadImgs(image_id)[0]
+        image_path = os.path.join(self.root, image_info['file_name'])
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        annotations = self.coco.loadAnns(self.coco.getAnnIds(imgIds=image_id))
+        boxes = np.array([ann['bbox'] for ann in annotations])
+        labels = np.array([ann['category_id'] for ann in annotations])
+
+        # Shift labels by +1
+        labels = labels + 1
+
+        # Convert COCO format boxes to Pascal VOC format before transformation
+        boxes = coco_to_voc(boxes)
+
+        if self.transform:
+            # Apply transformations to the image and bounding boxes
+            transformed = self.transform(image=image, bboxes=boxes, labels=labels)
+            image = transformed['image']
+            boxes = transformed['bboxes']
+            labels = transformed['labels']
+
+        boxes, labels = pad_to_fixed_size(boxes, labels)
+
+        # Convert boxes and labels to tensors
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        return image, boxes, labels
+
+    def __len__(self):
+        return len(self.image_ids)
+
+# Transformations using Albumentations
+transform = A.Compose([
+    A.Resize(width=image_size, height=image_size),
+    ToTensorV2()
+], bbox_params=A.BboxParams(format='pascal_voc', min_area=500, min_visibility=0.5, label_fields=['labels']))
+
+# Dataset instances
+dataset_path_x = "/dtu/blackhole/19/147257/coco_data/images/"
+
+train_dataset = CocoDataset(root=dataset_path_x + "train2017/", annotation=anns_file_train, transform=transform)
+val_dataset = CocoDataset(root=dataset_path_x + "val2017/", annotation=anns_file_val, transform=transform)
+
+# Dataloaders
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
 
 
-# Check if GPU is available and set the device accordingly
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Load and process categories
+with open(anns_file_train, 'r') as f:
+    dataset = json.loads(f.read())
+categories_df = pd.DataFrame(dataset['categories'])
+categories_df['label'] = categories_df['id'].astype(int) + 1
+categories_dict = dict(zip(categories_df['label'], categories_df['name']))
+categories_dict[0] = "background"
 
-# Initialize the model
-num_classes = 80  # Number of classes in COCO dataset
-model = ObjectDetectionModel(num_classes=num_classes).to(device)
 
-#Optimizer
-optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.0001)
 
-sample = train_dataset[28095]  
-image_tensor, bboxes, labels = sample
-print(f"1: {sample[0].shape}, 2: {sample[1].shape}, 3: {sample[2].shape}, {len(sample)}")
+# Load a random sample
+image, boxes, labels = get_random_sample(train_dataset)
 
-print(type(train_dataset))
+# Plot the image and bounding boxes
+image_name = f"sample_{random.randint(1, 10)}"
+save_plot(image, boxes, image_name)
 
-print(train_dataset[1][2].shape)
+print(image.shape)
 
-### Train
+# Print labels
+print("Labels and boxes:")
+for label, box in zip(labels, boxes):  # Use zip to iterate over labels and boxes together
+    if label.item() > 0:  # Check if the label is greater than 0 to ensure it's not a padded label
+        label_name = categories_dict.get(label.item(), "Unknown")
+        print(f" - {label_name}: {box.tolist()}")  # Convert the box to a list for readability
+    #else:
+    #    print(" - background or padding")
+
+
+#[203.9, 87.33, 107.09, 259.59, 1]
+
+print("init model...")
+
+# Model
+class ObjectDetector(Module):
+    def __init__(self, baseModel, numClasses):
+        super(ObjectDetector, self).__init__()
+        # initialize the base model and the number of classes
+        self.baseModel = baseModel
+        self.numClasses = numClasses
+        self.num_ftrs = baseModel.fc.in_features
+        # build the regressor head for outputting the bounding boxcoordinates
+        self.regressor = Sequential(
+            Linear(self.num_ftrs, 128),
+            ReLU(),
+            Linear(128, 64),
+            ReLU(),
+            Linear(64, 32),
+            ReLU(),
+            Linear(32, 4),
+            Sigmoid())
+        # build the classifier head to predict the class labels
+        self.classifier = Sequential(
+            Linear(self.num_ftrs, self.numClasses),
+        )
+        # set the classifier of our base model to produce outputs
+        # from the last convolution block
+        self.baseModel.fc = Identity()
+    def forward(self, x):
+        # pass the inputs through the base model and then obtain
+        # predictions from two different branches of the network
+        features = self.baseModel(x)
+        bboxes = self.regressor(features)
+        classLogits = self.classifier(features)
+        # return the outputs as a tuple
+        return (bboxes, classLogits)
+    
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# load the ResNet50 network
+resnet = models.resnet152(weights=models.ResNet152_Weights.DEFAULT).to(device)#resnet152(weights=ResNet152_Weights.IMAGENET1K_V1)
+# freeze all ResNet50 layers so they will *not* be updated during the
+# training process
+for param in resnet.parameters():
+    param.requires_grad = False
+# create our custom object detector model and flash it to the current
+# device
+model = ObjectDetector(resnet, num_classes)
+
+model = model.to(device)
+
+# Define loss functions
+regression_loss_fn = SmoothL1Loss()
+classification_loss_fn = CrossEntropyLoss()
+
+# Optimizer
+optimizer = Adam(model.parameters(), lr=1e-3)
+
+# Number of epochs
 num_epochs = 1
 
-# Loss functions
-regression_loss_fn = torch.nn.SmoothL1Loss()
-classification_loss_fn = torch.nn.CrossEntropyLoss()
+print("starting train...")
 
 # Training loop
-print("Starting training")
-# for epoch in range(num_epochs):
-#     train_loop = tqdm(train_loader, desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch')
+for epoch in range(num_epochs):
+    model.train()  # Set the model to training mode
+    for i, (images, bboxes, labels) in enumerate(train_loader):
+        images, bboxes, labels = images.to(device), bboxes.to(device), labels.to(device)
 
-#     for images, bbox_tuples, labels in train_loop:
-#         images = images.to(device)
-        
-#         bboxes = tuple(bbox.to(device) for bbox in bbox_tuples)
-#         labels = tuple(label.to(device) for label in labels)
+        # Forward pass
+        predicted_bboxes, predicted_labels = model(images)
 
-#         pred_bboxes, pred_labels = model(images)
+        # Compute losses
+        bbox_loss = regression_loss_fn(predicted_bboxes, bboxes)
+        class_loss = classification_loss_fn(predicted_labels, labels)
 
-#         regression_loss = regression_loss_fn(pred_bboxes, bboxes)
-#         classification_loss = classification_loss_fn(pred_labels, labels)
-#         total_loss = regression_loss + classification_loss
+        # Combine losses
+        total_loss = bbox_loss + class_loss
 
-#         optimizer.zero_grad()
-#         total_loss.backward()
-#         optimizer.step()
+        # Backward pass and optimize
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
 
-#         train_loop.set_postfix(loss=total_loss.item())
+        if i % 10 == 0:  # Print loss every 10 batches
+            print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{len(train_loader)}], Total Loss: {total_loss.item()}")
